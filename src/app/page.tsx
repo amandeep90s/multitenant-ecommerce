58b32291348a618d0edfe98010f09a3f075e7e95
Amandeep Singh <programmeramandeep@gmail.com>
2025-04-25 10:17:49 +0530

    feat: Customization in global css and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
 	return (
-		<div>
-			<Button variant={"default"}>Hello World</Button>
-			<p className="text-rose-500">I am Amandeep Singh</p>
+		<div className="flex flex-col gap-y-4 p-4">
+			<div>
+				<Button variant={"elevated"}>I am a button</Button>
+			</div>
+
+			<div>
+				<Input placeholder="Enter your name" defaultValue={"Aman"} />
+			</div>
+
+			<div>
+				<Progress value={50} />
+			</div>
+
+			<div>
+				<Textarea placeholder="Enter your message" value={"Hello World"} />
+			</div>
+
+			<div>
+				<Checkbox />
+			</div>
 		</div>
 	);
 }
