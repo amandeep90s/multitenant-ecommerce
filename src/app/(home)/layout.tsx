b71c4070060d464fc5574016d9193c6b07d07f57
Amandeep Singh <programmeramandeep@gmail.com>
2025-04-27 20:07:52 +0530

    feat: Add About, Contact, Features, Layout, Navbar, and Pricing pages with UI components

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import { Navbar } from "./navbar";
+
+interface LayoutProps {
+	children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Navbar />
+
+			{children}
+		</div>
+	);
+};
+
+export default Layout;
