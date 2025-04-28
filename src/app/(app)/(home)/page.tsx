4def6d4aedc3bc57719c2b5cc6e57cd068d02691
Amandeep Singh <programmeramandeep@gmail.com>
2025-04-28 19:18:08 +0530

    feat: Update package.json scripts for type generation and enhance Home page to fetch categories data

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,31 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
-export default function Home() {
-	return (
-		<div className="flex flex-col gap-y-4 p-4">
-			<div>
-				<Button variant={"elevated"}>I am a button</Button>
-			</div>
+export default async function Home() {
+	const payload = await getPayload({
+		config: configPromise,
+	});
 
-			<div>
-				<Input placeholder="Enter your name" defaultValue={"Aman"} />
-			</div>
+	const data = await payload.find({
+		collection: "categories",
+	});
 
-			<div>
-				<Progress value={50} />
-			</div>
-
-			<div>
-				<Textarea placeholder="Enter your message" value={"Hello World"} />
-			</div>
-
-			<div>
-				<Checkbox />
-			</div>
-		</div>
-	);
+	return <div>{JSON.stringify(data, null, 2)}</div>;
 }
