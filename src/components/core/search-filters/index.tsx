afbf971f649ca764612f9452766876983682a0be
Amandeep Singh <programmeramandeep@gmail.com>
2025-04-29 14:14:09 +0530

    feat: Enhance category management with new dropdown and search filters, update payload types, and integrate categories into layout

---
+++ src/components/core/search-filters/index.tsx
@@ -0,0 +1,15 @@
+import { Categories } from "./categories";
+import { SearchInput } from "./search-input";
+
+interface SearchFiltersProps {
+	data: unknown;
+}
+
+export const SearchFilters = ({ data }: SearchFiltersProps) => {
+	return (
+		<div className="flex flex-col gap-4 px-4 lg:px-12 py-8 border-b w-full">
+			<SearchInput />
+			<Categories data={data} />
+		</div>
+	);
+};
