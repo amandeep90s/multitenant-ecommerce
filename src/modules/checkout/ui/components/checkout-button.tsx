0230e46a1be35c50b186c0d22565309090b5821a
Amandeep Singh <programmeramandeep@gmail.com>
2025-05-14 22:17:01 +0530

    feat: Add CheckoutButton component and integrate it into Navbar for cart functionality

---
+++ src/modules/checkout/ui/components/checkout-button.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { cn, generateTenantUrl } from "@/lib/utils";
+import { useCart } from "@/modules/checkout/hooks/use-cart";
+import { ShoppingCartIcon } from "lucide-react";
+import Link from "next/link";
+
+interface CheckoutButtonProps {
+  className?: string;
+  hideIfEmpty?: boolean;
+  tenantSlug: string;
+}
+
+export const CheckoutButton = ({
+  className,
+  hideIfEmpty,
+  tenantSlug,
+}: CheckoutButtonProps) => {
+  const { totalItems } = useCart(tenantSlug);
+
+  if (hideIfEmpty && totalItems === 0) {
+    return null;
+  }
+
+  return (
+    <Button variant={"elevated"} asChild className={cn("bg-white", className)}>
+      <Link href={`${generateTenantUrl(tenantSlug)}/checkout`}>
+        <ShoppingCartIcon /> {totalItems > 0 ? totalItems : ""}
+      </Link>
+    </Button>
+  );
+};
--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -1,10 +1,28 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { generateTenantUrl } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { ShoppingCartIcon } from "lucide-react";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
+
+const CheckoutButton = dynamic(
+  () =>
+    import("@/modules/checkout/ui/components/checkout-button").then(
+      (mod) => mod.CheckoutButton,
+    ),
+  {
+    ssr: false,
+    loading: () => (
+      <Button disabled className="bg-white">
+        <ShoppingCartIcon className="text-black" />
+      </Button>
+    ),
+  },
+);
 
 interface NavbarProps {
   slug: string;
@@ -33,6 +51,7 @@ export const Navbar = ({ slug }: NavbarProps) => {
           )}
           <p className="text-xl">{data.name}</p>
         </Link>
+        <CheckoutButton hideIfEmpty tenantSlug={slug} />
       </div>
     </nav>
   );
@@ -43,6 +62,9 @@ export const NavbarSkeleton = () => {
     <nav className="h-20 border-b bg-white font-medium">
       <div className="mx-auto flex h-full max-w-(--breakpoint-xl) items-center justify-between px-4 lg:px-12">
         <div />
+        <Button disabled className="bg-white">
+          <ShoppingCartIcon className="text-black" />
+        </Button>
       </div>
     </nav>
   );
